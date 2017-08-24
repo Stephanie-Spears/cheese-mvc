389b5bae549dcc3ab0c5f3915b8ad2bf22f6e070
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-08-24 06:35:02 -0700

    update cheese to accept Cheese objects instead of hashmaps--temporary fix added for bug which allows an empty remove parameter to pass and then crash

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,69 +1,75 @@
 package org.launchcode.cheesemvc.controllers;
 
+import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
+import org.launchcode.cheesemvc.models.CheeseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-//import java.util.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-//import java.util.List;
+import java.util.*;
 
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
-    //    static ArrayList<String> cheeses = new ArrayList<>();
-    static HashMap<String, String> cheeses = new HashMap<>();
 
-    // Request path: /cheese--because of 'cheese' above
-    @RequestMapping(value = "")
+    @RequestMapping(value = "") // Request path: /cheese--because of 'cheese' above
     public String index(Model model) {
-        // pass data object, hashMap, to view
-        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
+        model.addAttribute("cheeses", CheeseData.getAll()); // pass data object (ArrayList of Cheese class) to view, via CheeseData class method getAll()
         return "cheese/index"; //path to templates directory that is specifically for the cheese controller
     }
 
-    // controller method to display a form
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+//        model.addAttribute("cheeseTypes", CheeseType.values());
         return "cheese/add";
     }
 
-    //handler to process the form
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, String cheeseDescription) {//@requestparam tells spring to look for a requets parameter with the same name as the method parameter and insert it into this method call. Needs to match the html variable for spring to be able to find it
-
-        cheeses.put(cheeseName, cheeseDescription);
-
-
-
-        // redirect to /cheese --which was specified as root beforehand, so leaving this empty sends us to root
-        return "redirect:";
+    public String processAddCheeseForm(@ModelAttribute @Valid Cheese newCheese, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Cheese");
+            return "cheese/add";
+        }
+        CheeseData.add(newCheese);
+        return "redirect:";//leaving this empty redirects to root, which was specified as /cheese previously
     }
 
     // controller method to display form
-    @RequestMapping(value = "delete", method= RequestMethod.GET)
-    public String displayDeleteCheeseForm(Model model){
-        model.addAttribute("title", "Delete Cheese");
-        model.addAttribute("cheeses", cheeses);
-        return "cheese/delete";
+    @RequestMapping(value = "remove", method= RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", CheeseData.getAll());
+        return "cheese/remove";
     }
 
 
     //handler to process the form
-    @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public String processDeleteCheeseForm(@RequestParam ArrayList<String> cheeseNames) {
-        for (String cheese : cheeseNames) {
-            cheeses.remove(cheese);
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam(required=false) int[] cheeseIds, Model model) {
+        if (cheeseIds == null){
+            model.addAttribute("title", "Remove Cheese");
+            model.addAttribute("cheeses", CheeseData.getAll());
+            model.addAttribute("errors", "You can't delete nothin' FOO!");
+            return "cheese/remove";
+        }
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
         return "redirect:";
+
     }
 
 }
